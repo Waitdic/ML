d770426531c615da5ec9244431baca9b1ad0f357
Danis Yakupov <waitdic161616@gmail.com>
2022-04-14 18:06:19 +0300

    fix

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,1 +1,28 @@
-print("hello world")
+import pandas
+import matplotlib.pyplot as plt
+
+names = ['sex', 'length', 'diameter', 'height', 'whole', 'shucked', 'viscera', 'shell', 'rings']
+data = pandas.read_csv('abalone.data', names=names)
+# print(data)
+
+# data["rings"].hist()
+# plt.show()
+
+# clear dataset
+data = data.drop('sex', axis=1)
+
+# search correlation
+correlation = data.corr()
+
+X = data.drop('rings', axis=1).values
+Y = data['rings'].values
+
+
+print(X)
+print(Y)
+
+def splitTrainTest(data, percent):
+    trainData = []
+    trainDataLen = round(len(data) * (percent / 100))
+    for row in data:
+        trainData
