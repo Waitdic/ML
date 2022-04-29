c7e568e52f452ef81f14fcb9b560911ec8dbaeb5
Danis Yakupov <waitdic161616@gmail.com>
2022-04-29 19:16:58 +0300

    fix

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,5 +1,7 @@
+from turtle import distance
 import pandas
 import matplotlib.pyplot as plt
+import numpy as np
 
 names = ['sex', 'length', 'diameter', 'height', 'whole', 'shucked', 'viscera', 'shell', 'rings']
 data = pandas.read_csv('abalone.data', names=names)
@@ -12,17 +14,24 @@ data = pandas.read_csv('abalone.data', names=names)
 data = data.drop('sex', axis=1)
 
 # search correlation
-correlation = data.corr()
+correlation = data.corr().drop('rings', axis=0)
 
 X = data.drop('rings', axis=1).values
 Y = data['rings'].values
 
+# print(X)
+# print(Y)
 
-print(X)
-print(Y)
+new_data_point = np.array(correlation['rings'])
+distances = np.linalg.norm(X - new_data_point, axis=1)
 
-def splitTrainTest(data, percent):
-    trainData = []
-    trainDataLen = round(len(data) * (percent / 100))
-    for row in data:
-        trainData
+k = 3
+nearest_neighbor_ids = distances.argsort()[:k]
+nearest_neighbor_rings = Y[nearest_neighbor_ids]
+
+
+# def splitTrainTest(data, percent):
+#     trainData = []
+#     trainDataLen = round(len(data) * (percent / 100))
+#     for row in data:
+#         trainData
